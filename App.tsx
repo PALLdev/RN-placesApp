897001b405ec1f4d1370c46d1e0dca8a75ada1e5
PALLdev <pblolara21@gmail.com>
2022-08-16 04:55:40 -0400

    added navigation and colors

--- App.tsx
+++ App.tsx
@@ -1,20 +1,68 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import {
+  createNativeStackNavigator,
+  NativeStackNavigationProp,
+} from "@react-navigation/native-stack";
+
+import AllPlaces from "./screens/AllPlaces";
+import AddPlaces from "./screens/AddPlaces";
+import IconButton from "./components/ui/IconButton";
+import { Colors } from "./constants/colors";
+
+type RootStackParamList = {
+  AllPlaces: undefined;
+  AddPlace: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
+  const pressAddIconHandler = (
+    navigation: NativeStackNavigationProp<RootStackParamList>
+  ) => {
+    navigation.navigate("AddPlace");
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>OHOW pq se apoaga er</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: Colors.primary500,
+            },
+            headerTintColor: Colors.gray700,
+            contentStyle: {
+              backgroundColor: Colors.gray700,
+            },
+          }}
+        >
+          <Stack.Screen
+            name="AllPlaces"
+            component={AllPlaces}
+            options={({ navigation }) => ({
+              title: "Lugares favoritos",
+              headerRight: ({ tintColor }) => (
+                <IconButton
+                  icon="add-circle-outline"
+                  color={tintColor}
+                  size={30}
+                  onPress={pressAddIconHandler.bind(null, navigation)}
+                />
+              ),
+            })}
+          />
+          <Stack.Screen
+            name="AddPlace"
+            component={AddPlaces}
+            options={{
+              title: "Agrega un lugar",
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- components/Places/PlacesList.tsx
+++ components/Places/PlacesList.tsx
@@ -1,4 +1,5 @@
 import { FlatList, StyleSheet, Text, View } from "react-native";
+import { Colors } from "../../constants/colors";
 import { Place } from "../../models/place";
 import PlaceItem from "./PlaceItem";
 
@@ -40,5 +41,6 @@ const styles = StyleSheet.create({
   },
   fallbackText: {
     fontSize: 16,
+    color: Colors.primary100,
   },
 });
